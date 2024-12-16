f73394608a519a42c6be7e44bcc58d2c3e18a9bc
r0man22 <roman.mammadov22@gmail.com>
2024-12-16 14:39:46 -0500

    how can i read a file linebyline

---
+++ linebylineread/001.sh
@@ -0,0 +1,21 @@
+
+#read without changing spaces and tabs within the line
+while IFS= read -r line; do
+	printf '%s\n' "$line"
+done < "$file"
+
+#changing spaces and tabs
+while read -r line; do
+	printf '%s\n' "$line"
+done < "$file"
+
+#assign columns from file to variable
+while read -r first second phone; do
+	printf '%s\n' "$second"
+done < "$file"
+
+
+#/etc/passwd
+while IFS=: read -r user pass uid gid gecos home shell; do
+	printf '%s: %s\n' "$user" "$shell"
+done < /etc/passwd
